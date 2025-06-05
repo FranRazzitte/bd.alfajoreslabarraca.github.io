c6c5c23f993b629f4b817f1b973d4ec11cf8e364
FranRazzitte <98547220+FranRazzitte@users.noreply.github.com>
2025-06-05 09:36:14 -0300

    Add categorias.js

---
+++ categorias.js
@@ -0,0 +1,56 @@
+const categorias = [
+  {
+    nombre: 'Chocolates',
+    colorLight: '#4d3b00',
+    colorDark: '#ffc400',
+    ampliable: false
+  },
+  {
+    nombre: 'Maicena',
+    colorLight: '#b49f59',
+    colorDark: '#ffe07a',
+    ampliable: false
+  },
+  {
+    nombre: 'Avena',
+    colorLight: '#554c00',
+    colorDark: '#ffe600',
+    ampliable: false
+  },
+  {
+    nombre: 'Merengue',
+    colorLight: '#008035',
+    colorDark: '#00ff6a',
+    ampliable: false
+  },
+  {
+    nombre: 'Frutales',
+    colorLight: '#860c0c',
+    colorDark: '#ff4b4b',
+    ampliable: false
+  },
+  {
+    nombre: 'Integrales',
+    colorLight: '#5f5f5f',
+    colorDark: '#c3c3c3',
+    ampliable: false
+  },
+  {
+    nombre: 'Saludables',
+    colorLight: '#003b03',
+    colorDark: '#97ff84',
+    ampliable: false
+  },
+  {
+    nombre: 'Cajas',
+    colorLight: 'black',
+    colorDark: 'white',
+    ampliable: false
+  },
+  {
+    nombre: 'Todos',
+    colorLight: 'var(--clr-general)',
+    colorDark: '#deff0b',
+    ampliable: true
+  }
+];
