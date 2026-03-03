6ef3386f32ae79df70fbfc8dff5b14878dac402e
FranRazzitte <98547220+FranRazzitte@users.noreply.github.com>
2026-03-03 01:51:01 -0300

    Update & Add files

--- js/carousel.js
+++ js/carousel.js
@@ -1,16 +1,21 @@
 const carousel = [
     {
         'id' : 1,
+        'src' : '/img/carousel/4.png',
+        'href' : 'https://www.instagram.com/p/DVXEGNGEUj_/'
+    },
+    {
+        'id' : 2,
         'src' : '/img/carousel/1.png',
         'href' : 'https://alfajoreslabarraca.com.ar/productos/?p=ddl-sin-azucar'
     },
     {
-        'id' : 2,
+        'id' : 3,
         'src' : '/img/carousel/2.png',
         'href' : 'https://alfajoreslabarraca.com.ar/productos/?p=pistacho'
     },
     {
-        'id' : 3,
+        'id' : 4,
         'src' : '/img/carousel/3.png',
         'href' : 'https://www.instagram.com/alfajoreslabarraca/'
     }
