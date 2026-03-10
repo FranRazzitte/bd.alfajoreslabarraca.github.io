e9f43ae3bb535987f2c85408839ced5d65ba3537
FranRazzitte <98547220+FranRazzitte@users.noreply.github.com>
2026-03-10 02:36:51 -0300

    Update files

--- js/eventos.js
+++ js/eventos.js
@@ -3,6 +3,7 @@
 const proximosEventos = [
   {
     titulo: '¡Volvemos a Expo Cupcakes!',
+    tituloAlter: 'Expo Cupcakes',
     diaI: 14,
     diaF: 15,
     mes: 3,
@@ -11,7 +12,21 @@ const proximosEventos = [
     horaF: '20',
     ubicacion: 'La Rural | Pabellón Ocre',
     desc: '¡Volvemos a Expo Cupcakes! Este 14 y 15 de marzo te esperamos de 13 a 20 horas en el Pabellón Ocre de La Rural. Vas a poder desgustar varios de nuestros sabores más exquisitos y algunas sorpresas especiales. ¿Te lo vas a perder?',
-    entrada: '{[https://www.laruralticket.com.ar/event/expocupcakesyreposteria-marzo2026]Adquirí tus entradas}',
+    imagenes: 'expo-cupcakes2026',
     iframe: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.9638795660626!2d-58.4209327!3d-34.5797805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb59cd6452553%3A0x6b521307d135059a!2sLa%20Rural!5e0!3m2!1ses-419!2sar!4v1772509628887!5m2!1ses-419!2sar'
+  },
+  {
+    titulo: '¡Primer Salón del Alfajor 2026!',
+    tituloAlter: 'Salón del Alfajor',
+    diaI: 17,
+    diaF: 19,
+    mes: 4,
+    año: 2026,
+    horaI: '12',
+    horaF: '20',
+    ubicacion: 'Predio Floral de Escobar',
+    desc: '¡Estamos orgullosos de anunciar que vamos a formar parte de la primera edición del Salón del Alfajor! Esta se realizará en el histórico Predio Floral de Escobar los días 17, 18 y 19 de abril, de 12 a 20hrs. ¡Te esperamos!',
+    imagenes: 'salon-del-alfajor2026',
+    iframe: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13177.717458821338!2d-58.79292774738921!3d-34.33954904850781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bb61a4d84c286b%3A0x807fdacdeaa64396!2sMateo%20Gelves%201050%2C%20B1625%20Bel%C3%A9n%20de%20Escobar%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1773120792564!5m2!1ses-419!2sar'
   }
 ];
